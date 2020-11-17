f5b33f665ef94f76a5a828ce662d106ffb08e4c5
eauod.17 <douae.elalj@gmail.com>
2020-11-17 19:48:55 +0100

    open chanpion

--- includes/virtual_machine.h
+++ includes/virtual_machine.h
@@ -15,7 +15,9 @@
 
 # include <stdio.h>
 # include <stdlib.h>
-#include "op.h"
+# include <fcntl.h>
+# include <string.h>
+# include "op.h"
 # include "../libft/headers/libft.h"
 
 # define FALSE 0
@@ -26,8 +28,29 @@ typedef struct	s_input_data
 	int		players_counter;
 	char	*names[MAX_PLAYERS];
 	int		ids[MAX_PLAYERS];
+	int		fd[MAX_PLAYERS];
 }				t_input_data;
 
-int ft_read_players(int argc, char **av, t_input_data *bloc, int *nbr_cycles);
+
+typedef struct		playrs_s
+{
+ header_t			header;
+ unsigned char		*exec_code;
+}					playrs_t;
+
+int		ft_read_players(int argc, char **av, t_input_data *bloc, int *nbr_cycles);
+
+int		ft_add_ids(t_input_data *bloc);
+void	ft_read_player_name(int idx, char **av, t_input_data *bloc);
+int		ft_read_player_numb(int idx, char **av, t_input_data *bloc);
+int		ft_read_dump(int i, char **av, int *nbr_cycles);
+void	print_input(t_input_data *bloc, int *nbr_cycles);
+
+
+void	ft_read_champion(int fd, header_t *data, playrs_t *playrs);
+void	ft_open_champion(t_input_data bloc, header_t data, playrs_t playrs);
+void	ft_fd_players(t_input_data	*bloc);
+int		ft_convert_num(char *temp);
+void	ft_exit(char *str);
 
 #endif
