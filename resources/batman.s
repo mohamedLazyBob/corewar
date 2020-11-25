5b06dd0b41a55359cb3f8332888a442ee614d424
mohamed lazybob <mzaboub@student.1337.ma>
2020-11-25 09:58:21 +0100

    changed how to exit and free

--- resources/batman.s
+++ resources/batman.s
@@ -2,7 +2,7 @@
 .comment    "This city needs me"
 
 #izjmp %:loop
-and %03, %:live, r1
+and %03, :live, r1
 loop:
         sti r1, %:live, %1
 live:
--- src/main_vm_parse.c
+++ src/main_vm_parse.c
@@ -6,11 +6,26 @@
 /*   By: mzaboub <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/16 18:42:03 by mzaboub           #+#    #+#             */
-/*   Updated: 2020/11/24 11:58:44 by mzaboub          ###   ########.fr       */
+/*   Updated: 2020/11/25 09:53:22 by mzaboub          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/virtual_machine.h"
+
+/*
+*******************************************************************************
+*/
+
+void	ft_free_exit(char *str, void **buff, size_t size)
+{
+	int	i;
+
+	i = -1;
+	while (++i < size)
+		free(buff[i]);
+	ft_putendl(str);
+	exit(0);
+}
 
 /*
 *******************************************************************************
@@ -58,31 +73,24 @@ int		main(int ac, char **av)
 {
 	t_input_data	bloc;
 	int				nbr_cycles[2];
-	int				ret;
+	//int				ret;
 	t_header		data;
 	t_playrs		*playrs;
 
 	// if ac == 1;
 	ft_memset(&bloc, 0, sizeof(bloc));
 	ft_memset(bloc.nbr_cycles, -1, 2 * sizeof(int));
-	ret = ft_read_players(ac, av, &bloc);
-	if (ret == 1)
-		printf("wrong number of arguments\n");
-	else if (ret == 2)
-		printf("wrong number in ID values \n");
-	else
-	{
-		print_input(&(bloc), bloc.nbr_cycles);
-		// playrs is a tab of players (size == bloc.players_counter)
-		playrs = (t_playrs*)ft_memalloc(sizeof(t_playrs) * bloc.players_counter);
-		// open_champion fills playrs tab.
-		ft_open_champion(bloc, playrs);
-	//	ft_init_arena(bloc, playrs);
-		ft_temp(bloc, playrs);
-		// init processes (all of them)
-		// play the game
-		// announce the winner
-		// exit
-	}
+	ft_read_players(ac, av, &bloc);
+	//print_input(&(bloc), bloc.nbr_cycles);
+	
+	playrs = (t_playrs*)ft_memalloc(sizeof(t_playrs) * bloc.players_counter);
+	ft_open_champion(bloc, playrs);
+
+	ft_temp(bloc, playrs);
+	// init processes (all of them)
+	// play the game
+	// announce the winner
+	// exit
+	
 	return (0);
 }
