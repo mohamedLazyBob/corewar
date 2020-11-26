79307e7e6810ce1ad4707ce579cfb8ab93180ad4
dino <woo.dong.7.wd@gmail.com>
2020-11-26 12:48:48 +0100

    modify operation function

--- src/ft_operation_add.c
+++ src/ft_operation_add.c
@@ -6,7 +6,7 @@
 /*   By: del-alj <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/20 10:14:10 by del-alj           #+#    #+#             */
-/*   Updated: 2020/11/25 18:51:20 by del-alj          ###   ########.fr       */
+/*   Updated: 2020/11/26 12:10:46 by del-alj          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ void	ft_operation_and(t_process *process)
 
 	types_byte = process->arena[process->pc + 1];
 	ft_get_args_type(*process, types_byte, parameters);
-	if (ft_strcmp((const char*)parameters, "ERR") == 0)
+	if (ft_strcmp((const char*)parameters, "ER") == 0)
 	{
 		printf("champion operation args error\n");
 		exit(0);
@@ -39,14 +39,15 @@ void	ft_operation_add(t_process *proc)
 	unsigned char	parameters[3];
 	unsigned int	types_byte;
 
-	types_byte = proc->arena[++proc->pc];
+	types_byte = proc->arena[proc->pc];
 	ft_get_args_type(*proc, types_byte, parameters);
-	if (ft_strcmp((const char*)parameters, "ERR") == 0)
+	if (ft_strcmp((const char*)parameters, "ER") == 0)
 		ft_exit("champion operation args error");
 	proc->regestries[proc->arena[proc->pc + 3]] = \
 	proc->regestries[proc->arena[proc->pc + 1]] + \
 	proc->regestries[proc->arena[proc->pc + 2]];
 	proc->carry = (proc->regestries[proc->arena[proc->pc + 3]] == 0) ? 1 : 0;
+	proc->pc += 3;
 }
 
 /*
@@ -58,19 +59,21 @@ void	ft_operation_sub(t_process *proc)
 	unsigned char	parameters[3];
 	unsigned int	types_byte;
 
-	types_byte = proc->arena[++proc->pc];
+	types_byte = proc->arena[proc->pc];
 	ft_get_args_type(*proc, types_byte, parameters);
-	if (ft_strcmp((const char*)parameters, "ERR") == 0)
+	if (ft_strcmp((const char*)parameters, "ER") == 0)
 		ft_exit("champion operation args error");
 	proc->regestries[proc->arena[proc->pc + 3]] = \
 	proc->regestries[proc->arena[proc->pc + 1]] - \
 	proc->regestries[proc->arena[proc->pc + 2]];
 	proc->carry = (proc->regestries[proc->arena[proc->pc + 3]] == 0) ? 1 : 0;
+	proc->pc += 3;
 }
 
 /*
 ********************************************************************************
 */
+
 void	ft_int_to_str(int var, char *str)
 {
 	str[3] = (unsigned int)(var);
@@ -78,6 +81,10 @@ void	ft_int_to_str(int var, char *str)
 	str[1] = (unsigned int)(var & 0x00ff0000) >> 16;
 	str[0] = (unsigned int)(var & 0xff000000) >> 24;
 }
+
+/*
+********************************************************************************
+*/
 
 void	ft_operation_st(t_process *proc)
 {
@@ -120,6 +127,7 @@ void	ft_operation_sti(t_process *proc)
 	unsigned char	parameters[3];
 	int				temp;
 	int 			var;
+	char			str[4];
 
 	var = 0;
 	temp = proc->pc - 1;
@@ -129,9 +137,25 @@ void	ft_operation_sti(t_process *proc)
 	if (ft_strcmp((const char*)parameters, "ER") == 0)
 		ft_exit("champion operation args error");
 	if (parameters[1] == T_IND)
-		proc->pc = temp + (ft_convert_num(proc->arena + (proc->pc + 1), 4) % \
+		temp = temp + (ft_convert_num(proc->arena + (proc->pc + 1), 4) % \
 				IDX_MOD);
-	temp = temp + ((proc->arena[proc->pc + 1] + proc->arena[proc->pc + 2]) %\
-			IDX_MOD);
-	ft_memcpy(proc->arena + temp, proc->arena + proc->pc, 2);
+	else
+	{
+			if (parameters[1] == T_REG && parameters[2] == T_REG)
+				temp = temp + ((proc->regestries[proc->arena[proc->pc + 1]] + \
+						proc->regestries[proc->arena[proc->pc + 2]]) % IDX_MOD);
+			else if (parameters[1] == T_REG && parameters[2] == T_DIR)
+				temp = temp + ((proc->regestries[proc->arena[proc->pc + 1]] + \
+					ft_convert_num(proc->arena + (proc->pc + 2), 2)) % IDX_MOD);
+			else if (parameters[1] == T_DIR && parameters[2] == T_REG)
+				temp = temp + ((ft_convert_num(proc->arena + (proc->pc + 1), 2)\
+					+ proc->regestries[proc->arena[proc->pc + 3]]) % IDX_MOD);
+			else if (parameters[1] == T_DIR && parameters[2] == T_DIR)
+				temp = temp + (ft_convert_num(proc->arena + (proc->pc + 1), 2)\
+				+ (ft_convert_num(proc->arena + (proc->pc + 3), 2)) % IDX_MOD);
+			else
+				;
+	}
+	ft_int_to_str(proc->regestries[proc->arena[proc->pc]], str);
+	ft_memcpy(proc->arena + temp, (const void *)str, 2);
 }
--- src/ft_operation_ld.c
+++ src/ft_operation_ld.c
@@ -6,7 +6,7 @@
 /*   By: del-alj <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/23 14:01:18 by del-alj           #+#    #+#             */
-/*   Updated: 2020/11/24 14:31:39 by del-alj          ###   ########.fr       */
+/*   Updated: 2020/11/26 11:54:51 by del-alj          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,21 +20,24 @@ void	ft_operation_ld(t_process *proc)
 {
 	unsigned int	types_byte;
 	unsigned char	parameters[3];
+	int				temp;
 
-	types_byte = proc->arena[++proc->pc];
+	temp = proc->pc - 1;
+	types_byte = proc->arena[proc->pc];
 	ft_get_args_type(*proc, types_byte, parameters);
-	if (ft_strcmp((const char*)parameters, "ERR") == 0)
+	if (ft_strcmp((const char*)parameters, "ER") == 0)
 		ft_exit("champion operation args error");
 	proc->pc++;
 	if (parameters[0] == T_DIR)
 	{
 		proc->regestries[proc->arena[proc->pc + 4]] = \
-						ft_convert_num(proc->arena + proc->pc, 4);
+									ft_convert_num(proc->arena + proc->pc, 4);
 		proc->pc += 4;
 	}
 	else
 	{
-		proc->pc = proc->pc + (ft_convert_num(proc->arena + proc->pc, 2) % IDX_MOD);
+		proc->regestries[proc->arena[proc->pc + 2]] = temp + \
+						(ft_convert_num(proc->arena + proc->pc, 2) % IDX_MOD);
 		proc->pc += 2;
 	}
 	proc->carry = (proc->regestries[proc->arena[proc->pc++]] == 0) ? 1 : 0;
@@ -44,25 +47,28 @@ void	ft_operation_ld(t_process *proc)
 ********************************************************************************
 */
 
- void	ft_operation_lld(t_process *proc)
+void	ft_operation_lld(t_process *proc)
 {
 	unsigned int	types_byte;
 	unsigned char	parameters[3];
+	int				temp;
 
-	types_byte = proc->arena[++proc->pc];
+	temp = proc->pc - 1;
+	types_byte = proc->arena[proc->pc];
 	ft_get_args_type(*proc, types_byte, parameters);
-	if (ft_strcmp((const char*)parameters, "ERR") == 0)
+	if (ft_strcmp((const char*)parameters, "ER") == 0)
 		ft_exit("champion operation args error");
 	proc->pc++;
 	if (parameters[0] == T_DIR)
 	{
 		proc->regestries[proc->arena[proc->pc + 4]] = \
-			ft_convert_num(proc->arena + proc->pc, 4);
+									ft_convert_num(proc->arena + proc->pc, 4);
 		proc->pc += 4;
 	}
 	else
 	{
-		proc->pc = proc->pc + ft_convert_num(proc->arena + proc->pc, 2);
+		proc->regestries[proc->arena[proc->pc + 2]] = temp + \
+						ft_convert_num(proc->arena + proc->pc, 2);
 		proc->pc += 2;
 	}
 	proc->carry = (proc->regestries[proc->arena[proc->pc++]] == 0) ? 1 : 0;
@@ -78,26 +84,27 @@ void	ft_operation_ldi(t_process *proc)
 	unsigned char	parameters[3];
 	int				temp;
 
-	types_byte = proc->arena[++proc->pc];
+	temp = proc->pc - 1;
+	types_byte = proc->arena[proc->pc];
 	ft_get_args_type(*proc, types_byte, parameters);
-	if (ft_strcmp((const char*)parameters, "ERR") == 0)
+	if (ft_strcmp((const char*)parameters, "ER") == 0)
 		ft_exit("champion operation args error");
 	proc->pc++;
 	if (parameters[0] == T_IND)
 	{
-		temp = proc->pc + (ft_convert_num(proc->arena + proc->pc, 2) % IDX_MOD);
-		proc->pc += 2;
+		temp = temp + (ft_convert_num(proc->arena + proc->pc, 4) % IDX_MOD);
+		proc->pc += 4;
 	}
 	else
 	{
-		temp = proc->pc + ((ft_convert_num(proc->arena + proc->pc, parameters[1]) +\
-				ft_convert_num(proc->arena + proc->pc, parameters[2])) % IDX_MOD);
+		temp = temp + ((ft_convert_num(proc->arena + proc->pc, parameters[1]) +\
+			ft_convert_num(proc->arena + proc->pc, parameters[2])) % IDX_MOD);
+		
 		proc->pc += parameters[0];
 	}
 	proc->pc += parameters[1];
 	proc->regestries[proc->arena[proc->pc++]] = temp;
 }
-
 /*
 ********************************************************************************
 */
@@ -108,20 +115,22 @@ void	ft_operation_lldi(t_process *proc)
 	unsigned char	parameters[3];
 	int				temp;
 
-	types_byte = proc->arena[++proc->pc];
+	temp = proc->pc - 1;
+	types_byte = proc->arena[proc->pc];
 	ft_get_args_type(*proc, types_byte, parameters);
-	if (ft_strcmp((const char*)parameters, "ERR") == 0)
+	if (ft_strcmp((const char*)parameters, "ER") == 0)
 		ft_exit("champion operation args error");
 	proc->pc++;
 	if (parameters[0] == T_IND)
 	{
-		temp = proc->pc + (ft_convert_num(proc->arena + proc->pc, 2) % IDX_MOD);
-		proc->pc += 2;
+		temp = temp + (ft_convert_num(proc->arena + proc->pc, 4) % IDX_MOD);
+		proc->pc += 4;
 	}
 	else
 	{
-		temp = proc->pc + (ft_convert_num(proc->arena + proc->pc, parameters[0]) +\
-				ft_convert_num(proc->arena + proc->pc, parameters[1]));
+		temp = temp + (ft_convert_num(proc->arena + proc->pc, parameters[1]) +\
+				ft_convert_num(proc->arena + proc->pc, parameters[2]));
+		
 		proc->pc += parameters[0];
 	}
 	proc->pc += parameters[1];
