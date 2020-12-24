58708f384517e8fbd817873147e6c32981d9fef3
dino <woo.dong.7.wd@gmail.com>
2020-12-24 08:07:57 +0100

    for pull

--- src/ft_operations_ld.c
+++ src/ft_operations_ld.c
@@ -69,9 +69,13 @@ void	ft_operation_ld(t_process *proc)
 void	ft_operation_lld(t_process *proc)
 {
 	unsigned char	parameters[3];
-	int				temp;
+	int	args[3];
 
-	temp = proc->pc - 1;
+	// ft_printf("debug -- inside ld\n");
+	// exit(0);
+	ft_memset(args, 0, 3);
+	ft_memset(parameters, 0, 3);
+	proc->op_pc = proc->pc - 1;
 	ft_get_args_type(proc, proc->arena[0][proc->pc], parameters);
 	if (ft_strcmp((const char*)parameters, "ER") == 0)
 	{
@@ -79,17 +83,20 @@ void	ft_operation_lld(t_process *proc)
 					ft_sizeof_params(proc, parameters)) % MEM_SIZE;
 			return ;
 	}
-/**/
-	if (parameters[0] == T_DIR)
-		proc->regestries[proc->arena[0][(proc->pc + 4) % IDX_MOD] - 1] = \
-									ft_reverse_endianness(proc->arena[0] + proc->pc, 4);
-	else
-		proc->regestries[proc->arena[0][(proc->pc + 2) % IDX_MOD] - 1] = temp + \
-						ft_reverse_endianness(proc->arena[0] + proc->pc, 2);
-	proc->carry = (proc->regestries[proc->arena[0][proc->pc++] - 1] == 0) ? 1 : 0;
-/**/
-	proc->pc = (proc->pc + \
-					ft_sizeof_params(proc, parameters)) % MEM_SIZE;
+	args[0] = ft_parse_args(proc, parameters[0]);
+	args[1] = ft_parse_args(proc, parameters[1]);
+	if (args[1] < 1 || 16 < args[1])
+		return;
+//	ft_printf("debug -- after args[0]: %d, args[1]: %d\n", proc->regestries[1], args[1]);
+	args[0] = ft_get_argument_value(proc, args[0], parameters[0]);
+	//  ft_printf("\ndebug -- p%d args[0]: %d\n", proc->proc_id, args[0]);
+	proc->carry = (args[0] == 0) ? 1 : 0;
+	proc->regestries[args[1] - 1] = args[0];
+	// ft_printf("debug -- after args[0]: %d, carry: %d\n", args[0], proc->carry);
+
+	// proc->carry = (proc->regestries[args[1]] == 0) ? 1 : 0;
+	// ft_printf("dest: %d, and carry: %d\n", proc->regestries[args[1]], proc->carry);
+	mz_print_op(proc, parameters, args);
 	#if 0
 	// make this compilable whenever you want
 	mz_print_op(proc, parameters, args);
@@ -138,7 +145,7 @@ void	ft_operation_ldi(t_process *proc)
 
 #if 0
 /**/
-	if (parameters[0] == T_IND)
+	if (parameters[0] == T_IN
 		temp = temp + (ft_reverse_endianness(proc->arena[0] + proc->pc, 4) % IDX_MOD);
 	else
 		temp = temp + ((ft_reverse_endianness(proc->arena[0] + proc->pc, ft_size(parameters[1], 2)) +\
