1adff5df9cf3b9c8dabaa56fa3ec0290154d9c54
dino <woo.dong.7.wd@gmail.com>
2020-12-25 07:59:34 +0100

    for pull

--- src/main_vm_parse.c
+++ src/main_vm_parse.c
@@ -13,7 +13,7 @@
 #include "../includes/virtual_machine.h"
 
 t_input_data		*g_input_bloc;
-t_process	*g_procs_head;
+
 
 /*
 *******************************************************************************
