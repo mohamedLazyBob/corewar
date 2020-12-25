86dde3a48ad22f94016bbadc781971fe45d11c1b
mohamed lazybob <mzaboub@student.1337.ma>
2020-12-25 08:39:16 +0100

    changed the printing whitspace from 4 spaces to {%4d}

--- src/mz_mode_verbos.c
+++ src/mz_mode_verbos.c
@@ -25,13 +25,13 @@ void	mz_print_op(t_process *proc, unsigned char args_types[3], int value[3])
 	if ((proc->next_inst ==  AND) || (proc->next_inst ==  OR) || (proc->next_inst ==  XOR))
 	{
 		// ft_printf("debug -- print xor -- %d, %d, r%d\n", value[0], value[1], value[2]);
-		ft_printf("P    %d | %s %d %d r%d\n", proc->proc_id, \
+		ft_printf("P %4d | %s %d %d r%d\n", proc->proc_id, \
 			g_op_tab[proc->next_inst].op_name, \
 			value[0], value[1], value[2]);
 	}
 	else if ((proc->next_inst ==  LDI) || (proc->next_inst == STI))
 	{
-		char string[2][50] = {"P    %d | sti r%d %d %d\n", \
+		char string[2][50] = {"P %4d | sti r%d %d %d\n", \
 				    "P    %d | ldi %d %d r%d\n"};
 		ft_printf(((proc->next_inst == STI) ? string[0] : string[1]), \
 				proc->proc_id, value[0], value[1], value[2]);
@@ -62,30 +62,30 @@ void	mz_print_op(t_process *proc, unsigned char args_types[3], int value[3])
 	}
 	else if (proc->next_inst == LLD)
 	{
-		ft_printf("P    %d | lld %d r%d\n", proc->proc_id, value[0], value[1]);
+		ft_printf("P %4d | lld %d r%d\n", proc->proc_id, value[0], value[1]);
 	}
 	else if (proc->next_inst == LLDI)
 	{
-		ft_printf("P    %d | lldi %d %d r%d\n", proc->proc_id, value[0], value[1], value[2]);
+		ft_printf("P %4d | lldi %d %d r%d\n", proc->proc_id, value[0], value[1], value[2]);
 	}
 	else if (proc->next_inst == LD)
 	{
-		ft_printf("P    %d | ld %d r%d\n", proc->proc_id, value[0], value[1]);
+		ft_printf("P %4d | ld %d r%d\n", proc->proc_id, value[0], value[1]);
 	}
 	else if (proc->next_inst == ST)
 	{
 		// in case value[1] negative need to cast whit short i dont know if afect positive numbers 
 		//ft_printf("P    %d | st r%d %d\n", proc->proc_id, value[0], (short)value[1]);
-		ft_printf("P    %d | st r%d %d\n", proc->proc_id, value[0], value[1]);
+		ft_printf("P %4d | st r%d %d\n", proc->proc_id, value[0], value[1]);
 	}
 	else if (proc->next_inst == ADD)
 	{
-		ft_printf("P    %d | add r%d r%d r%d\n", proc->proc_id, \
+		ft_printf("P %4d | add r%d r%d r%d\n", proc->proc_id, \
 								value[0], value[1], value[2]);
 	}
 	else if (proc->next_inst == SUB)
 	{
-		ft_printf("P    %d | sub r%d r%d r%d\n", proc->proc_id, \
+		ft_printf("P %4d | sub r%d r%d r%d\n", proc->proc_id, \
 			value[0], value[1], value[2]);
 	}
 	// this shouldn't be here, it should be called after the execution 
@@ -106,9 +106,9 @@ void	mz_l_fork_mode_verbos(t_process *proc, int par1, int new_pc)
 		return ;	
 	}
 	if (proc->next_inst == FORK)
-		ft_printf("P    %d | fork %d (%d)\n", proc->proc_id, par1, new_pc);
+		ft_printf("P %4d | fork %d (%d)\n", proc->proc_id, par1, new_pc);
 	else
-		ft_printf("P    %d | lfork %d (%d)\n", proc->proc_id, par1, new_pc);
+		ft_printf("P %4d | lfork %d (%d)\n", proc->proc_id, par1, new_pc);
 	// if ((g_input_bloc->flags[VERBOS_1] & 16 || \
 	// 	g_input_bloc->flags[VERBOS_2] & 16))
 	// 	mz_print_pc_movements(proc);
