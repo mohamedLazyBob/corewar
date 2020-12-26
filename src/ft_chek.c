9d39767fd6f66a96ac93add0dcf322993027f321
dino <woo.dong.7.wd@gmail.com>
2020-12-26 19:47:18 +0100

    now game stop it ned just put how is the player. i ll do it monday

--- src/ft_chek.c
+++ src/ft_chek.c
@@ -17,24 +17,21 @@ extern  t_process		*g_procs_head;
 
 static  int ft_check_carriage(t_process *carriage, t_game *game_params)
 {
-    if (game_params->cycles_to_die <= 0 || carriage->process_live == 0 || carriage->process_live >= game_params->cycles_to_die)
-    {
-        //  write(2, "die\n", 4);
-        //  ft_printf(" *               *********                %d    %d\n", carriage->proc_id, carriage->operation_live);
-        return (0);
-    }
-    if (carriage->process_live > 0 && (game_params->total_live_counter >= NBR_LIVE || game_params->checks_counter == MAX_CHECKS))
-        carriage->process_live = 0;
+  if (game_params->cycles_to_die <= 0 || carriage->process_live == 0 || carriage->process_live >= game_params->cycles_to_die)
+    return (0);
+else
     return (1);
 }
 
+/*
+** ****************************************************************************
+**
+*/
+
 static void ft_check_cycle(t_game *game_params)
 {
-    // ft_printf("	===					total checks counter: %d\n", game_params->checks_counter);
-
     if (game_params->total_live_counter >= NBR_LIVE || game_params->checks_counter == MAX_CHECKS)
     {
-        // game_params->checks_counter = 0;
         game_params->cycles_to_die -= CYCLE_DELTA;
         if (game_params->checks_counter > 0)
             ft_printf("Cycle to die is now %d\n", game_params->cycles_to_die);
@@ -46,6 +43,27 @@ static void ft_check_cycle(t_game *game_params)
 		game_params->cycles_to_die = 1;
     game_params->total_live_counter = 0;
 }
+
+/*
+** ****************************************************************************
+**
+*/
+
+static void    ft_clear_lives(t_process **proc)
+{
+    t_process *temp;
+    temp = *proc;
+    while (temp)
+    {
+        temp->process_live = 0;
+        temp = temp->next;
+    }
+}
+
+/*
+** ****************************************************************************
+**
+*/
 
 static void ft_kill_carriage(t_process **carriage)
 {
@@ -63,6 +81,11 @@ static void ft_kill_carriage(t_process **carriage)
         free(temp);
     }
 }
+
+/*
+** ****************************************************************************
+**
+*/
 
 void        ft_check(t_process **proc, t_game **game_params)
 {
@@ -90,21 +113,30 @@ void        ft_check(t_process **proc, t_game **game_params)
     
         while (carriage && carriage->next != NULL)
         {
+            // ft_printf("++++++++++++debug heeere\n");
             if (!(ft_check_carriage(carriage->next, (*game_params))))
                 ft_kill_carriage(&carriage);
             else
                 carriage = carriage->next;
         }
+        // for the laste proc
+        if (*proc && carriage != NULL && carriage->next == NULL && \
+				(!(ft_check_carriage(carriage, (*game_params)))))
+        {
+            ft_printf("Contestant %d, \"name\", has won !\n", (*proc)->player_id * -1);
+            free((*proc));
+            (*proc) = NULL;
+            (carriage) = NULL;
+        }
 		if (proc && (*game_params)->cycles_to_die < 0)
 		{
             free((*proc));
             (*proc) = NULL;
-            (proc) = NULL;
+            //(proc) = NULL;
 		}
 
    }
     ft_check_cycle((*game_params));
+    ft_clear_lives(proc);
     // debug_print_procs_list(*proc);
-
-    
 }
