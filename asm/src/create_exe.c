f0fe5bf870f86745e02e4c116ac23cfec808ffa6
Tareq Bareich <tbareich@e1r10p13.1337.ma>
2021-02-27 11:13:45 +0100

    add -a option

--- asm/src/create_exe.c
+++ asm/src/create_exe.c
@@ -6,25 +6,28 @@
 /*   By: tbareich <tbareich@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/23 07:12:21 by tbareich          #+#    #+#             */
-/*   Updated: 2021/02/23 07:16:34 by tbareich         ###   ########.fr       */
+/*   Updated: 2021/02/27 11:09:27 by tbareich         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <asm.h>
 #include <fcntl.h>
 
-int	create_exe(char *str)
+void	create_exe(t_asm *asm_info)
 {
 	char	*tmp;
-	int		exe_fd;
 
-	tmp = check_file_name_handler(str);
+	if (asm_info->exe_fd == 1)
+	{
+		asm_info->exe_fd =  1;
+		return ;
+	}
+	tmp = check_file_name_handler(asm_info->file_name);
 	*tmp = 0;
-	tmp = ft_strjoin(str, ".cor");
-	exe_fd = open(tmp, O_RDWR | O_CREAT, 0777);
-	if (exe_fd < 0)
+	tmp = ft_strjoin(asm_info->file_name, ".cor");
+	asm_info->exe_fd = open(tmp, O_RDWR | O_CREAT, 0777);
+	if (asm_info->exe_fd < 0)
 		error("File: can't create .cor file");
 	ft_printf("Writing output program to %s\n", tmp);
 	ft_memdel((void **)&tmp);
-	return (exe_fd);
 }
---
+++ asm/src/handlers/command_handler.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   command_handler.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tbareich <tbareich@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/02/27 10:37:48 by tbareich          #+#    #+#             */
+/*   Updated: 2021/02/27 11:13:18 by tbareich         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <asm.h>
+#include <fcntl.h>
+
+static int	check_option(char *str)
+{
+	if (ft_strequ(str, "-a"))
+		return (1);
+	usage();
+	return (0);
+}
+
+void		command_handler( t_asm *asm_info, int ac, char **av)
+{
+	if (ac < 2 || ac > 3)
+		usage();
+	if (ac == 2)
+	{
+		if (ft_strequ(av[1], "--help") || ft_strequ(av[1], "-h"))
+			usage();
+		check_file_name_handler(av[1]);
+		asm_info->asm_fd = open(av[1], O_RDONLY);
+		if (asm_info->asm_fd < 0)
+			error(FILE_DOES_NOT_EXIST);
+		asm_info->file_name = av[1];
+	}
+	else
+	{
+		asm_info->minus_a_option = check_option(av[1]);
+		asm_info->exe_fd = 1;
+		check_file_name_handler(av[2]);
+		asm_info->asm_fd = open(av[2], O_RDONLY);
+		if (asm_info->asm_fd < 0)
+			error(FILE_DOES_NOT_EXIST);
+		asm_info->file_name = av[2];
+	}
+}
